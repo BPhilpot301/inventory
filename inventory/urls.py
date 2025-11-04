bf8d4526f852d61a5db4cc82c7c279038b39e701
BPhilpot301 <britnphilpot43@outlook.com>
2025-11-03 21:55:15 -0600

    second

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inventory/', views.inventory_pages, name='inventory_page')
-    
+    path('inventory/', views.inventory_pages, name='inventory_page'),
+    path('add_book/', views.add_book_view, name='add_book'),
+    path('edit_book/<int:id>/', views.edit_book_view, name= 'edit_book'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Book
+from.forms import BookForm
+
+
 # Create your views here.
 def inventory_pages(request):
     book_list = Book.objects.all()
     return render(request, 'inventory/inventory.html', {'books_list':book_list})
+
+def add_book_view(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('inventory_page')
+    else:
+        form = BookForm()
+
+    return render(request, 'inventory/add_book.html', {'form':form})
+
+def edit_book_view(request, id):
+    book = Book.objects.get(id=id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('inventory_page')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'inventory/edit_book.html', {'form':form})
+
